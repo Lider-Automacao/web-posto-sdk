c008360a5678e47d6c3bc0794bf20bcd7e03c7e2
Raiane Campos <raianejanu@hotmail.com>
2025-09-05 15:42:28 -0300

    fix: corrige exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ export * from './centro-custo-cliente';
 export * from './cliente';
 export * from './cliente-unidade-negocio-request';
 export * from './contato-cliente';
+export * from './empresa';
 export * from './funcionario';
 export * from './titulo-receber';
 export * from './venda';
